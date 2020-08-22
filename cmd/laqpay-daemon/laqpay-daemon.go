52d628d88a3979a172392b1c9f1fdd6e13fceb90
LaQ Pay <59437025+laqpay@users.noreply.github.com>
2020-08-23 04:37:42 +0500

    Update laqpay-daemon.go

--- cmd/laqpay-daemon/laqpay-daemon.go
+++ cmd/laqpay-daemon/laqpay-daemon.go
@@ -21,7 +21,7 @@ import (
 
 var (
 	// Version of the node. Can be set by -ldflags
-	Version = "0.1.1"
+	Version = "0.1.2"
 	// Commit ID. Can be set by -ldflags
 	Commit = ""
 	// Branch name. Can be set by -ldflags
